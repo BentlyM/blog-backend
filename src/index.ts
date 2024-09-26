6a068f6814ceaf5c21deaaa759fbef09290bc780
BentlyM <metayerbently@gmail.com>
2024-09-26 12:48:20 -0400

    "Updated package-lock.json and src/index.ts files"

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
 import session from 'express-session';
 import passport from 'passport';
 import {default as registerRouter} from './routes/register';
@@ -6,12 +6,17 @@ import {default as loginRouter} from './routes/login';
 import {default as postsRouter} from './routes/posts';
 import {default as commentsRouter} from './routes/comments'
 import loggerMiddleware from './middleware/logger';
+import cors from 'cors';
 
 
 const app = express();
 
 const hostname = '0.0.0.0';
 const PORT = +(process.env.PORT || 8080);
+
+const corsOptions = {
+    origin: '*',
+}
 
 app.use(session({
     secret: 'cat',
@@ -22,6 +27,7 @@ app.use(passport.session());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors(corsOptions));
 
 app.use(loggerMiddleware);
 
@@ -29,6 +35,19 @@ app.use('/', registerRouter);
 app.use('/', loginRouter);
 app.use('/', postsRouter);
 app.use('/', commentsRouter);
+
+app.use('*', (req, res) => {
+	res.status(404).json({
+		error: 'Resource not found',
+	});
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500).json({
+      error: 'Internal server error',
+    });
+  
+  });
 
 app.listen(PORT, hostname, ()=>{
     console.log(`listening on server http://${hostname}:${PORT}`);
